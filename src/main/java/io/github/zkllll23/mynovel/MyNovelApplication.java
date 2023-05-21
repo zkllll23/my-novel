45692a40c23a8d2c525d67781e4aa55c626523c2
zkllll23 <133302305@qq.com>
2023-05-21 15:58:29 +0800

    feat: 初始化项目

---
+++ src/main/java/io/github/zkllll23/mynovel/MyNovelApplication.java
@@ -0,0 +1,13 @@
+package io.github.zkllll23.mynovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyNovelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/zkllll23/mynovel/MyNovelApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zkllll23.mynovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNovelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
