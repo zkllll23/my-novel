dcecb502aa03215a0774804bda409eb5662a7718
zkllll23 <133302305@qq.com>
2023-05-22 20:16:24 +0800

    feat(core): 跨域配置

---
+++ src/main/java/io/github/zkllll23/mynovel/core/config/CorsProperties.java
@@ -0,0 +1,19 @@
+package io.github.zkllll23.mynovel.core.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+/**
+ * @author zkllll23
+ * @date 2023/5/22
+ */
+@ConfigurationProperties(prefix = "my-novel.cors")
+@Data
+public class CorsProperties {
+    /**
+     * 允许跨域的域名
+     */
+    private List<String> allowOrigins;
+}
