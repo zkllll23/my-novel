cc9d1a72ffb603e17d8738bbc57fe3c1a6b2dd55
zkllll23 <133302305@qq.com>
2023-05-24 21:32:38 +0800

    feat: 集成和配置本地缓存Caffeine

---
+++ src/main/java/io/github/zkllll23/mynovel/core/config/CacheConfig.java
@@ -0,0 +1,44 @@
+package io.github.zkllll23.mynovel.core.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import io.github.zkllll23.mynovel.core.constant.CacheConsts;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 缓存配置类
+ *
+ * @author zkllll23
+ * @date 2023/5/23
+ */
+public class CacheConfig {
+
+    /**
+     * Caffeine 缓存管理器
+     */
+    @Bean
+    public CacheManager caffeineCacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+
+        List<CaffeineCache> caches = new ArrayList<>(CacheConsts.CacheEnum.values().length);
+        for (CacheConsts.CacheEnum c : CacheConsts.CacheEnum.values()) {
+            if (c.isLocal()) {
+                Caffeine<Object, Object> caffeine = Caffeine.newBuilder().recordStats().maximumSize(c.getMaxSize());
+                if (c.getTtl() > 0) {
+                    caffeine.expireAfterWrite(Duration.ofSeconds(c.getTtl()));
+                }
+                caches.add(new CaffeineCache(c.getName(), caffeine.build()));
+            }
+        }
+
+        cacheManager.setCaches(caches);
+        return cacheManager;
+    }
+
+}
---
+++ src/main/java/io/github/zkllll23/mynovel/core/constant/ApiRouterConsts.java
@@ -0,0 +1,23 @@
+package io.github.zkllll23.mynovel.core.constant;
+
+/**
+ * API路由常量
+ *
+ * @author zkllll23
+ * @date 2023/5/21
+ */
+public class ApiRouterConsts {
+
+    /**
+     * API请求路径前缀
+     */
+    public static final String API_URL_PREFIX = "/api";
+
+    /**
+     * 私有化构造方法
+     */
+    private ApiRouterConsts() {
+        // 防止该类被实例化
+        throw new UnsupportedOperationException("This class cannot be instantiated.");
+    }
+}
