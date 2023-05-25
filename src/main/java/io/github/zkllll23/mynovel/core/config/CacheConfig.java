30577d0bd0254db35e7dd5156f13e0ed0eaba46c
zkllll23 <133302305@qq.com>
2023-05-25 14:21:53 +0800

    feat: 集成和配置分布式缓存Redis

--- src/main/java/io/github/zkllll23/mynovel/core/config/CacheConfig.java
+++ src/main/java/io/github/zkllll23/mynovel/core/config/CacheConfig.java
@@ -6,10 +6,17 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 缓存配置类
@@ -23,6 +30,7 @@ public class CacheConfig {
      * Caffeine 缓存管理器
      */
     @Bean
+    @Primary
     public CacheManager caffeineCacheManager() {
         SimpleCacheManager cacheManager = new SimpleCacheManager();
 
@@ -39,6 +47,35 @@ public class CacheConfig {
 
         cacheManager.setCaches(caches);
         return cacheManager;
+    }
+
+    /**
+     * Redis 缓存管理器
+     */
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
+
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues().prefixCacheNameWith(CacheConsts.REDIS_CACHE_PREFIX);
+
+        Map<String, RedisCacheConfiguration> cacheMap = new LinkedHashMap<>(CacheConsts.CacheEnum.values().length);
+        for (CacheConsts.CacheEnum c : CacheConsts.CacheEnum.values()) {
+            if (c.isRemote()) {
+                if (c.getTtl() > 0) {
+                    cacheMap.put(c.getName(), RedisCacheConfiguration.defaultCacheConfig().disableCachingNullValues()
+                            .prefixCacheNameWith(CacheConsts.REDIS_CACHE_PREFIX).entryTtl(Duration.ofSeconds(c.getTtl())));
+                } else {
+                    cacheMap.put(c.getName(), RedisCacheConfiguration.defaultCacheConfig().disableCachingNullValues()
+                            .prefixCacheNameWith(CacheConsts.REDIS_CACHE_PREFIX));
+                }
+            }
+        }
+
+        RedisCacheManager redisCacheManager = new RedisCacheManager(redisCacheWriter, defaultCacheConfig, cacheMap);
+        redisCacheManager.setTransactionAware(true);
+        redisCacheManager.initializeCaches();
+        return redisCacheManager;
     }
 
 }
